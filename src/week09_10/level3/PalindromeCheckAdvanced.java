2a6ac64bc038a28a8f0949502e709008985fec30
Praghya <pg6408@srmist.edu.in>
2026-05-06 09:29:29 +0530

    Added PalindromeCheckAdvanced program for week09_10 level3

---
+++ src/week09_10/level3/PalindromeCheckAdvanced.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PalindromeCheckAdvanced {
+
+    static boolean isPalindrome(String text) {
+
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        boolean result = isPalindrome(text);
+
+        if (result) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}

98f71b6275af7bcba77fc4eca4b9d19bf34e86e2
Praghya <pg6408@srmist.edu.in>
2026-05-06 09:02:04 +0530

    Added CompareStrings program for week09_10 level1

---
+++ src/week09_10/level1/CompareStrings.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CompareStrings {
+
+    static boolean compareStrings(String text1, String text2) {
+
+        if (text1.length() != text2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < text1.length(); i++) {
+            if (text1.charAt(i) != text2.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String text1 = sc.next();
+
+        System.out.print("Enter second string: ");
+        String text2 = sc.next();
+
+        boolean result1 = compareStrings(text1, text2);
+        boolean result2 = text1.equals(text2);
+
+        System.out.println("Using charAt(): " + result1);
+        System.out.println("Using equals(): " + result2);
+    }
+}
